6d8adf5ae7454bf6250b75848bb4edf9502ee451
shyam <12shyam02gupta@gmail.com>
2024-06-27 19:28:59 +0530

    DropBox complete

--- frontend/src/Context/Reducer.js
+++ frontend/src/Context/Reducer.js
@@ -1,33 +1,31 @@
-const Reducer = (state ,action) =>{
+const Reducer = (state, action) => {
     switch (action.type) {
-
         case 'OPEN_LOGIN':
             return { ...state, openLogin: true };
         case 'CLOSE_LOGIN':
             return { ...state, openLogin: false };
-        
         case 'START_LOADING':
-            return { ...state, loading:true };
+            return { ...state, loading: true };
         case 'END_LOADING':
-            return { ...state, loading:false };
-        
+            return { ...state, loading: false };
         case 'UPDATE_ALERT':
-            return {...state,alert:action.payload}
-
+            return { ...state, alert: action.payload };
         case 'UPDATE_PROFILE':
-            return {...state,profile:action.payload}
-        
-            
-
+            return { ...state, profile: action.payload };
+        case 'UPDATE_IMAGES':
+            return { ...state, images: [...state.images, action.payload] };
+        case 'DELETE_IMAGE':
+            return {
+                ...state,
+                images: state.images.filter((image) => image !== action.payload),
+            };
         case 'UPDATE_USER':
-            localStorage.setItem('currentUser',JSON.stringify(action.payload))
-            return {...state,currentUser:action.payload}
-    
+            localStorage.setItem('currentUser', JSON.stringify(action.payload));
+            return { ...state, currentUser: action.payload };
         default:
-            throw new Error("No matched action")
+            console.error(`No matched action: ${action.type}`);
+            return state;
     }
-
-    
-}
+};
 
 export default Reducer;
